d5d277941ab8c5fce90d42728489aa5ac91c70e0
Puleng80 <pmogotsi80@gmail.com>
2022-12-13 13:11:20 -0800

    task

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,8 +2,10 @@
 
 /**
  * _putchar - Entry Point
+ * @c: The character to print
  *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
  */
 
 int _putchar(char c)

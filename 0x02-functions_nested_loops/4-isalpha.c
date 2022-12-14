8731185f788bce4f181b1e0e1ba3c36dc37146a2
Puleng80 <pmogotsi80@gmail.com>
2022-12-14 08:58:42 -0800

    task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,5 +9,13 @@
 
 int _isalpha(int c)
 {
-	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+	if
+		((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
 }

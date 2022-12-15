3b12778f01d401c73977b8af2a1f89c486bdf032
Puleng80 <pmogotsi80@gmail.com>
2022-12-15 13:05:18 -0800

    tasks

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,13 +19,13 @@ void print_triangle(int size)
 			{
 				for (j = i; j < size; j++)
 				{
-					   _putchar(' ');
+					_putchar(' ');
 				}
 					for (j = 1; j <= i; j++)
-				{
-					  _putchar('#');
-				}
-				  _putchar('\n');
+					{
+						_putchar('#');
+					}
+					_putchar('\n');
 			}
 	}
 }
